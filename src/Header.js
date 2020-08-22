8f3c3f338a4d364cc5a45d5999f70ffde2b132f7
Abidin K <abidinkofficial@gmail.com>
2020-08-22 09:27:45 +0300

    cra cleanup, header implemented

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return(
+        <header>
+            <a href='/' className='header-logo'>bit<span className='header-logo-bold'>koyin!</span></a>
+            <button className='header-search-button'><i className='material-icons'>search</i></button>
+        </header>
+    );
+}
+
+export default Header;
